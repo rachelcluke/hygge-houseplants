3a7d092405229d132937e9c16180a140a61ea39c
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-01-31 12:09:02 +0000

    product_info.html added | also added to urls.py + views.py

--- products/views.py
+++ products/views.py
@@ -9,10 +9,10 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-def product_detail(request, product_id):
+def product_info(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
     context = {'product': product}
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/product_info.html', context)
