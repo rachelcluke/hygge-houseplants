5ab59ccd7afd6337d0d5e39c0044110cd0f4cd65
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-02 19:11:11 +0000

    search results added

--- products/views.py
+++ products/views.py
@@ -19,8 +19,9 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-
+                products = products.annotate(lower_name=sortkey.lower())
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
