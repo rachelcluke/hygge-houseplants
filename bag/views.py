5360e01c36daae2c58fa0363e5c5897a62ab0ecf
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-05 14:39:25 +0000

    toasts added to modifiying + removing bag content events

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -18,6 +18,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.product_name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.product_name} to your bag')
@@ -28,13 +29,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the respective product to the respective number """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.product_name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.product_name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -43,9 +47,12 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ Remove the respective item from the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
+
     try:
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.product_name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
