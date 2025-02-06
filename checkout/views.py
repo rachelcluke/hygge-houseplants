a89b5036513d809fc557ee04d72cdcf868503d4c
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-06 12:47:39 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QpUDDF9uUBrkCnyx8qam52fOQ5sM2pfY50rlQ7kg8DgJNJao0zZoc0JGoiS9tXYBWhwgPsFJXiY666tOQmwqEb300QJLN6P6c',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
