5e6868306141f29f26860db06d88db29afe3f335
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-09 15:16:57 +0000

    change static_url

--- hygge_houseplants/settings.py
+++ hygge_houseplants/settings.py
@@ -237,7 +237,7 @@ if 'USE_CLOUDINARY' in os.environ:
     MEDIAFILES_LOCATION = 'media_library/folders/media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    STATIC_URL = f'https://{CL_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{CL_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
