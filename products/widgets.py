fab71fb46ed4e68fbca7ddcf3324a5df6144d455
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-11 13:47:32 +0000

    NEW - widgets.py added (for edit product form)

---
+++ products/widgets.py
@@ -0,0 +1,11 @@
+# Source: https://github.com/django/django/blob/main/django/forms/templates/django/forms/widgets/clearable_file_input.html
+
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
