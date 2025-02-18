96d62bc4dcd93c8acdcb33ffb9890343bb7949e4
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-18 22:21:14 +0000

    TESTING - TestCheckoutModels - pass || views - fail

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .forms import OrderForm
+from .models import Order, OrderLineItem, Product
 
 class TestOrderForm(TestCase):
     """ Test Order Form in Checkout App """
@@ -22,9 +24,32 @@ class TestOrderForm(TestCase):
 class TestViews(TestCase):
     """ Test Views in Checkout App """
 
+    def setUp(self):
+        """ Create a user """
+        self.user = User.objects.create_user(
+            username="myUsername", password="myPassword12!!!", email="test00@test.com")
+
     # TODO - resolve
     def test_get_checkout_page(self):
         """ Test checkout.html view """
-        response = self.client.get('/checkout/')
+        self.client.login(username="myUsername", password="myPassword12!!!")
+        response = self.client.get('/checkout')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response,'checkout/checkout.html')
+    
+    # TODO - resolve
+    def test_get_checkout_success_page(self):
+        """ Test checkout_success.html view """
+        # user = User.objects.create_user(username="testUsername", password="myPass@495867", email="tester1@test.com")
+        self.client.login(username="myUsername", password="myPassword12!!!")
+        order = Order.objects.create(order_number=000, full_name='Yasmin Oh', phone_number = 29248, country='United Kingdom', town_or_city='x', street_address1='x' )
+        response = self.client.get(f'/checkout-success/{order.order_number}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'checkout/checkout_success.html')
+
+class TestCheckoutModels(TestCase):
+    """ Test Models in Checkout App """
+    def test_order_model(self):
+        """ Test Order model """
+        testorder = Order.objects.create(order_number=000, full_name='Yasmin Oh', phone_number = 29248, country='United Kingdom', town_or_city='x', street_address1='x' )
+        self.assertTrue(testorder)
