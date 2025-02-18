e59d89eae8b0bb3a2772cd94011c004336504137
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-18 19:48:10 +0000

    TESTING - test_get_profile_page() - pass

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,26 +1,18 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django.test.client import Client
-from .forms import UserProfileForm
 
-"""
-class LoginTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user('wedn01', 'pear@prg.com', 'jKs9bN!8j38')
 
-    def testLogin(self):
-        self.client.login(username='wedn01', password='jKs9bN!8j38')
-        response = self.client.get(reversed('login'))
-        self.assertEqual(response.status_code, 200)
-"""
-
-# TODO - resolve
 class TestViews(TestCase):
     """ Test Views in Profiles App """
 
+    def setUp(self):
+        """ Create a user """
+        self.user = User.objects.create_user(
+            username="myUsername", password="myPassword", email="test@test.com")
+
     def test_get_profile_page(self):
         """ Test profile.html view """
+        self.client.login(username="myUsername", password="myPassword")
         response = self.client.get('/profile/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response,'profiles/profile.html')
