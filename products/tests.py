14218075352753dd0d5bcc6ae9e03377daff167d
enyrl3 <83579803+enyrl3@users.noreply.github.com>
2025-02-18 13:44:14 +0000

    django tests views added

--- products/tests.py
+++ products/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
+from .models import Product
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
+    """ Test Views in Products App """
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
-
+    def test_get_products_page(self):
+        """ Test products.html view """
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'products/products.html')
+    
+    def test_get_products_info_page(self):
+        """ Test product_info.html view """
+        product = Product.objects.create(product_name='Test Product', product_description='x', price='2.00')
+        response = self.client.get(f'/products/{product.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_info.html')
